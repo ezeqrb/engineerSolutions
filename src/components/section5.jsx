189bee39a0cd0e39e2f669269c57e66e39ae4e3a
Ezequiel Romero Bertani <71280667+ezeqrb@users.noreply.github.com>
2021-09-13 12:45:53 -0300

    clean2

--- src/components/section5.jsx
+++ src/components/section5.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import Cards from './card'
-import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box'
 
 
